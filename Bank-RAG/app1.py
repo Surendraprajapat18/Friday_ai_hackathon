2b223b7f4237f9a6edbdae986b47eb2f26e07081
Surendra Prajapat <97840357+Surendraprajapat18@users.noreply.github.com>
2025-08-30 12:32:21 +0530

    Update app1.py

--- Bank-RAG/app1.py
+++ Bank-RAG/app1.py
@@ -33,7 +33,7 @@ client = httpx.Client(verify=False)
 llm = ChatOpenAI(
     base_url="https://genailab.tcs.in",
     model="azure_ai/genailab-maas-DeepSeek-V3-0324",
-    api_key="sk-ZMKZrlh-F2tYNrvuaWBj5A",
+    api_key="",
     http_client=client,
     temperature=0.3
 )
@@ -252,3 +252,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
